5ef04a1e892230698b961ad0359f6536156fe552
Francis Genet <58907@supinfo.com>
2013-03-18 17:50:26 -0700

    trunkstore account (need to add the PBX) + limits

--- api/accounts.php
+++ api/accounts.php
@@ -13,6 +13,8 @@ class Accounts {
     private $_settings = null;
     private $_auth_token = "";
     private $_account_id = "";
+    private $_realm = "";
+    private $_trunkstore_account_id = "";
 
     function options() {
         return;
@@ -46,8 +48,7 @@ class Accounts {
 
     private function _get_realm_random() {
         $random = Utils::get_random("", 8);
-
-        return $random . ".sip.2600hz.com";
+        $this->_realm = $random . ".sip.2600hz.com";
     }
 
     function __construct() {
@@ -59,11 +60,14 @@ class Accounts {
         $this->_get_auth_token();
     }
 
+    // Create main account
     private function _create_account($request_data) {
+        $this->_get_realm_random();
+
         $account_data = array(
             "data" => array(
                 "name" => "Test account",
-                "realm" => $this->_get_realm_random(),
+                "realm" => $this->_realm,
                 "timezone" => "America/Los_Angeles"
             )
         );
@@ -80,7 +84,58 @@ class Accounts {
         ));
 
         $response = json_decode(curl_exec($this->_curl));
-        return $response['data']['id'];
+        return $response->data->id;
+    }
+
+    // Create trunkstore account
+    private function _create_trunkstore_account($request_data) {
+        $trunkstore_account_data = array(
+            "data" => array(
+                "account" => array(
+                    "credits" => array(
+                        "prepay" => '0.00'
+                    ),
+                    "trunks" => '0',
+                    "inbound_trunks" => '0',
+                    "auth_realm" => $this->_realm
+                ),
+                "billing_account_id" => $this->_account_id,
+                "DIDs_Unassigned" => array(),
+                "servers" => array()
+            )
+        );
+
+        curl_setopt_array($this->_curl, array(
+            CURLOPT_CUSTOMREQUEST => "PUT",
+            CURLOPT_URL => "http://apps001-qa-fmt.2600hz.com:8000/v1/accounts/" . $this->_account_id . "/connectivity",
+            CURLOPT_POSTFIELDS => json_encode($trunkstore_account_data)
+        ));
+
+        $response = json_decode(curl_exec($this->_curl));
+        return $response->data->id;
+    }
+
+    private function _set_limits($request_data) {
+        $limits_data = array(
+            "data" => array(
+                "twoway_trunks" => 1,
+                "inbound_trunks" => 1
+            )
+        );
+
+        curl_setopt_array($this->_curl, array(
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_URL => "http://apps001-qa-fmt.2600hz.com:8000/v1/accounts/" . $this->_account_id . "/limits",
+            CURLOPT_POSTFIELDS => json_encode($limits_data)
+        ));
+
+        $response = json_decode(curl_exec($this->_curl));
+        print_r($response);
+    }
+
+    // Adding a credit card
+    private function _add_credit_card($request_data) {
+        $credit_card_data = array();
     }
 
     /**
@@ -94,7 +149,16 @@ class Accounts {
             $this->_account_id = $account_id;
         else
             throw new RestException(500, "Could not create the account");
-            
+
+        echo $account_id;
+
+        $trunkstore_account_id = $this->_create_trunkstore_account($request_data);
+        if ($trunkstore_account_id)
+            $this->_trunkstore_account_id = $trunkstore_account_id;
+        else
+            throw new RestException(500, "Could not create the trunkstore account");
+
+        $this->_set_limits($request_data);
     }
 }
 
