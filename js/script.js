34e2b4c9f0f70963ef153a4feafb1b628ac3586e
Jean-Roch Maitre <68488@supinfo.com>
2013-03-20 23:59:24 -0700

    Initial commit of Trunking.io using Francis' onboarding APIs

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 // JavaScript Document
 var THIS = this,
-    default_trunk_price = 29.99,
+    default_api_url = 'https://api.2600hz.com:8443/v1',
+    default_trunk_price = 6.99,
+    default_outbound_trunk_price = 24.99,
     default_amount_trunks = 1,
+    default_amount_outbound_trunks = 1,
     format_phone_number = function(phone_number) {
-        if(phone_number.substr(0,2) === "+1" && phone_number.length === 12) {
+        if(phone_number.substr(0,2) === '+1' && phone_number.length === 12) {
             phone_number = phone_number.replace(/(\+1)(\d{3})(\d{3})(\d{4})/, '$1 ($2) $3-$4');
         }
 
@@ -16,10 +19,17 @@ if($('.pay-as-you-go').size() > 0) {
         if(typeof(Storage)!=='undefined') {
             delete sessionStorage['trunking_trunks_data'];
 
+            var list_dids = [];
+
+            $('.number-line').each(function(k, v) {
+                list_dids.push($(v).data('phone_number'));
+            });
+
             sessionStorage.setItem('trunking_trunks_data', JSON.stringify({
-                price: $('#totalcost-number').data('price') || default_trunk_price,
+                price: $('#totalcost-number').data('price'),
                 money: $('#amount_money').data('amount') || 0,
-                dids: $('#amount_numbers').data('number') || 0
+                dids: $('#amount_numbers').data('number') || 0,
+                list_dids: list_dids
             }));
         }
     });
@@ -30,10 +40,18 @@ if($('.pay-trunks').size() > 0) {
         if(typeof(Storage)!=='undefined') {
             delete sessionStorage['trunking_trunks_data'];
 
+            var list_dids = [];
+
+            $('.number-line').each(function(k, v) {
+                list_dids.push($(v).data('phone_number'));
+            });
+
             sessionStorage.setItem('trunking_trunks_data', JSON.stringify({
-                price: $('#totalcost-number').data('price') || default_trunk_price,
-                trunks: $('#amount_twoway_trunks').data('number') || default_amount_trunks,
-                dids: $('#amount_numbers').data('number') || 0
+                price: $('#totalcost-number').data('price') || (default_trunk_price + default_outbound_trunk_price),
+                trunks: $('#amount_inbound_trunks').data('number') || default_amount_trunks,
+                outbound_trunks: $('#amount_twoway_trunks').data('number') || default_amount_outbound_trunks,
+                dids: $('#amount_numbers').data('number') || 0,
+                list_dids: list_dids
             }));
         }
     });
@@ -42,18 +60,25 @@ if($('.pay-trunks').size() > 0) {
 /* Wizard */
 if($('#onboarding_wrapper').size() > 0) {
     if(typeof(Storage)!=='undefined') {
-        var data_trunks = JSON.parse(sessionStorage.getItem('trunking_trunks_data')) || { price: default_trunk_price, trunks: default_amount_trunks, dids: 0 };
+        var data_trunks = JSON.parse(sessionStorage.getItem('trunking_trunks_data')) || { price: default_trunk_price + default_outbound_trunk_price, trunks: default_amount_trunks, outbound_trunks: default_amount_outbound_trunks, dids: 0 };
 
         if('money' in data_trunks) {
-            $('#amount_money').html('$' + data_trunks.money);
+            $('#totalcost-title').html('Total Cost');
+            $('#amount_money').html('$' + data_trunks.money)
+                              .parent().show();
             $('li #amount_twoway_trunks').parent().hide();
+            $('li #amount_inbound_trunks').parent().hide();
         }
         else {
-            $('#amount_twoway_trunks').html(data_trunks.trunks);
+            $('#totalcost-title').html('Monthly Cost');
+            $('#amount_twoway_trunks').html(data_trunks.outbound_trunks)
+                                      .parent().show();
+            $('#amount_inbound_trunks').html(data_trunks.trunks)
+                                      .parent().show();
             $('#amount_money').parent().hide();
         }
 
-        $('#totalcost-number').html('$' + data_trunks.price);
+        $('#totalcost-number').html('$' + parseFloat(data_trunks.price).toFixed(2));
         $('#amount_numbers').html(data_trunks.dids);
     }
 
@@ -259,6 +284,111 @@ if($('#onboarding_wrapper').size() > 0) {
         });
     });
 
+    $('#confirm-complete').on('click', function() {
+        if(typeof(Storage)!=='undefined') {
+            var trunk_data = JSON.parse(sessionStorage.getItem('trunking_trunks_data')),
+                sip_credentials = JSON.parse(sessionStorage.getItem('sip_credentials')),
+                company = $('#acct-name').val(),
+                email = $('#acct-email').val(),
+                name = $('#billing-name').val(),
+                first_name = name.substr(0, name.indexOf(' ')),
+                last_name = name.substr(name.indexOf(' '), name.length),
+                username = $('#acct-username').val()
+                password = $('#acct-password').val(),
+                country = $('#billing-country').val(),
+                extended_address = $('#billing-extended-address').val(),
+                locality = $('#billing-city').val(),
+                postal_code = $('#billing-zipcode').val(),
+                region = $('#billing-state').val(),
+                street_address = $('#billing-address1').val(),
+                cvv = $('#billing-cvv').val(),
+                expiration_date = $('#billing-expiration').val(),
+                number = $('#billing-cardnumber').val(),
+                data_form = {
+                    data: {
+                        braintree: {
+                            company: company,
+                            email: email,
+                            first_name: first_name,
+                            last_name: last_name,
+                            credit_card: {
+                                billing_address: {
+                                    country: country,
+                                    extended_address: extended_address,
+                                    first_name: first_name,
+                                    last_name: last_name,
+                                    locality: locality,
+                                    postal_code: postal_code,
+                                    region: region,
+                                    street_address: street_address
+                                },
+                                cardholder_name: name,
+                                cvv: cvv,
+                                expiration_date: expiration_date,
+                                make_default: true,
+                                number: number
+                            }
+                        },
+                        account: {
+                            name: company,
+                            available_apps: ['pbxs']
+                        },
+                        user: {
+                            apps: {
+                                'pbxs': {
+                                    'id': 'pbxs',
+                                    'label': 'PBX Connector',
+                                    'api_url': default_api_url,
+                                    'default': true
+                                }
+                            },
+                            credentials: $.md5(username+':'+password),
+                            email: email,
+                            first_name: first_name,
+                            last_name: last_name,
+                            priv_level: 'admin'
+                        },
+                        pbx: {
+                            list_dids: trunk_data.list_dids,
+                            auth: {
+                                auth_method: 'Password',
+                                auth_user: sip_credentials.username,
+                                auth_password: sip_credentials.password
+                            }
+                        }
+                    }
+                };
+
+            if('money' in trunk_data) {
+                data_form.data.trunks = {
+                    money: trunk_data.money
+                };
+            }
+            else {
+                data_form.data.trunks = {
+                    inbound: trunk_data.trunks,
+                    outbound: trunk_data.outbound_trunks
+                };
+            }
+
+            $.ajax({
+                type: 'POST',
+                dataType: 'json',
+                url: 'http://10.10.2.221:8888/trunking_io/api/accounts',
+                data: JSON.stringify(data_form),//data_form,
+                contentType: 'application/json',
+                crossDomain: true
+            }).success(function(msg){
+                console.log('success! ' + msg);
+            }).error(function(msg) {
+                console.log('error! ' + msg);
+            });
+        }
+        else {
+            alert('Browser doesn\'t support HTML5 Storage');
+        }
+    });
+
     THIS.initialize_wizard($('#onboarding_wrapper'));
 }
 
@@ -281,27 +411,27 @@ if($('#testimonial-section').size() > 0) {
 /* Twitter plugin */
 if($('#tweets-wrapper').size() > 0) {
     $('#tweets-wrapper').tweet({
-        username: "2600hertz",
-        join_text: "auto",
+        username: '2600hertz',
+        join_text: 'auto',
         avatar_size: 40,
         count: 2,
-        auto_join_text_default: "",
-        auto_join_text_ed: "",
-        auto_join_text_ing: "",
-        auto_join_text_reply: "",
-        auto_join_text_url: "",
-        loading_text: "loading tweets..."
+        auto_join_text_default: '',
+        auto_join_text_ed: '',
+        auto_join_text_ing: '',
+        auto_join_text_reply: '',
+        auto_join_text_url: '',
+        loading_text: 'loading tweets...'
     });
 }
 /* Tumblr plugin */
 if(typeof google !== 'undefined') {
-    google.load("feeds", "1");
+    google.load('feeds', '1');
 
     function OnLoad() {
         var feedControl = new google.feeds.FeedControl();
         feedControl.setNumEntries(1);
-        feedControl.addFeed("http://blog.2600hz.com/rss");
-        feedControl.draw(document.getElementById("blog-wrapper"));
+        feedControl.addFeed('http://blog.2600hz.com/rss');
+        feedControl.draw(document.getElementById('blog-wrapper'));
     }
 
     google.setOnLoadCallback(OnLoad);
@@ -309,13 +439,13 @@ if(typeof google !== 'undefined') {
 
 /* Beginning of our JS */
 var update_data_trunks = function(trunks_amount) {
-    var twoway_price = 6.99,
-        total_amount_twoway = (trunks_amount - parseInt($('#amount_twoway_trunks').attr('data-number') || 1)) * twoway_price,
+    var inbound_price = 6.99,
+        total_amount_inbound = (trunks_amount - parseInt($('#amount_inbound_trunks').attr('data-number') || 1)) * inbound_price,
         current_price = parseFloat($('#totalcost-number').attr('data-price')),
-        total_price = (current_price + total_amount_twoway).toFixed(2);
+        total_price = (current_price + total_amount_inbound).toFixed(2);
 
     $('#slider-amount').html(trunks_amount);
-    $('#amount_twoway_trunks').html(trunks_amount)
+    $('#amount_inbound_trunks').html(trunks_amount)
                               .attr('data-number', trunks_amount);
 
     $('#totalcost-number').html('$'+ total_price)
@@ -324,12 +454,12 @@ var update_data_trunks = function(trunks_amount) {
 
 var update_data_outboundTrunks = function(trunks_amount) {
     var twoway_price = 24.99,
-        total_amount_twoway = (trunks_amount - parseInt($('#amount_outbound_trunks').attr('data-number') || 1)) * twoway_price,
+        total_amount_twoway = (trunks_amount - parseInt($('#amount_twoway_trunks').attr('data-number') || 1)) * twoway_price,
         current_price = parseFloat($('#totalcost-number').attr('data-price')),
         total_price = (current_price + total_amount_twoway).toFixed(2);
 
-    $('#ouboundSlider-amount').html(trunks_amount);
-    $('#amount_outbound_trunks').html(trunks_amount)
+    $('#outboundSlider-amount').html(trunks_amount);
+    $('#amount_twoway_trunks').html(trunks_amount)
                               .attr('data-number', trunks_amount);
 
     $('#totalcost-number').html('$'+ total_price)
@@ -399,8 +529,8 @@ $('#select_amount').on('change', function() {
 
 $('#search-for-did-btn').on('click', function() {
     $('#popup').dialog({
-        title: "Select Phone Numbers",
-        width: "500px",
+        title: 'Select Phone Numbers',
+        width: '500px',
         resizable: false,
         modal: true
     });
@@ -415,8 +545,13 @@ $('#search-for-did-btn').on('click', function() {
 });
 
 $('#search-numbers').on('click', function() {
+    $('.text-notloading').hide();
+    $('.text-loading').show();
     $.getJSON('https://api.2600hz.com:8443/v1/phone_numbers', {prefix: parseInt($('.list-numbers #search-input').val()) || 415, quantity: 20}, function(response) {
         $('#results .number-selection').empty();
+
+        $('.text-notloading').show();
+        $('.text-loading').hide();
 
         if(response.data.length > 0) {
             $('#submit_numbers').show();
@@ -449,7 +584,7 @@ $('#submit_numbers').on('click', function() {
 
     $.each(list_numbers, function(k, v) {
         if($('.remove-did[data-phone_number="'+ v +'"]').size() === 0) {
-            $('#selected-numbers').append('<li class="number-line">'+THIS.format_phone_number(v)+'<a class="remove-did" data-phone_number="'+v+'" href="#">remove</a></li>');
+            $('#selected-numbers').append('<li class="number-line" data-phone_number="'+v+'">'+THIS.format_phone_number(v)+'<a class="remove-did" data-phone_number="'+v+'" href="#">remove</a></li>');
         }
     });
 
@@ -489,13 +624,22 @@ $(document).ready(function() {
     $.ajax({
         type: "GET",
         dataType: "json",
-        url: "http://10.10.2.218:8888/trunking_io/api/tempaccount/credentials",
+        url: "http://10.10.2.221:8888/trunking_io/api/tempaccount/credentials",
         contentType: "application/json",
         crossDomain: true
     }).done(function(msg){
         $('#generated-user').html(msg.data.username);
         $('#generated-password').html(msg.data.password);
         $('#ip-number').html(msg.data.ip);
+
+        if(typeof(Storage)!=='undefined') {
+            delete sessionStorage['sip_credentials'];
+
+            sessionStorage.setItem('sip_credentials', JSON.stringify({
+                username: msg.data.username,
+                password: msg.data.password
+            }));
+        }
     });
 
     //credentials registered
@@ -524,6 +668,8 @@ $(document).ready(function() {
                 });
                 clearInterval(thisInterval);
             }
+        }).error(function(msg) {
+            clearInterval(thisInterval);
         });
     },5000);
 
